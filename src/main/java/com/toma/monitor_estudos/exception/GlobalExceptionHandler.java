3f44211f6a2a7c3366acfaaf489a5cfea25335cd
Thiago Toma <thiagotoma99@gmail.com>
2026-07-22 15:33:55 -0300

    feat(api): improve global exception handling and validation responses

--- src/main/java/com/toma/monitor_estudos/exception/GlobalExceptionHandler.java
+++ src/main/java/com/toma/monitor_estudos/exception/GlobalExceptionHandler.java
@@ -1,41 +1,69 @@
 package com.toma.monitor_estudos.exception;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(SessaoInvalidaException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErroResponse> handleSessaoInvalida(SessaoInvalidaException ex, HttpServletRequest request){
-        ErroResponse erroResponse = new ErroResponse(
-                LocalDateTime.now(),
-                HttpStatus.BAD_REQUEST.value(),
-                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+        ErroResponse erroResponse = buildErroResponse(
+                HttpStatus.BAD_REQUEST,
                 ex.getMessage(),
-                request.getRequestURI()
+                request
         );
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erroResponse);
     }
 
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<ErroResponse> handleErrosNaoTratados(Exception ex, HttpServletRequest request) {
-        ErroResponse erroResponse = new ErroResponse(
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErroResponse> handleEntityNotFoundException(EntityNotFoundException ex, HttpServletRequest request) {
+        ErroResponse erroResponse = buildErroResponse(
+                HttpStatus.NOT_FOUND,
+                ex.getMessage(),
+                request
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erroResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErroResponse> handleValidationException(MethodArgumentNotValidException ex, HttpServletRequest request){
+
+        String mensagem = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(";\n "));
+        ErroResponse erroResponse = buildErroResponse(HttpStatus.BAD_REQUEST,mensagem, request);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erroResponse);
+    }
+
+    private ErroResponse buildErroResponse(HttpStatus status, String mensagem, HttpServletRequest request) {
+        return  new ErroResponse(
                 LocalDateTime.now(),
-                HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
-                "Ocorreu um erro interno inesperado no servidor. Por favor, tente novamente mais tarde.",
+                status.value(),
+                status.getReasonPhrase(),
+                mensagem,
                 request.getRequestURI()
         );
 
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErroResponse> handleErrosNaoTratados(Exception ex, HttpServletRequest request) {
+
+        ErroResponse erroResponse = buildErroResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "Ocorreu um erro interno inesperado no servidor. Por favor, tente novamente mais tarde.",
+                request
+        );
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(erroResponse);
     }
 
